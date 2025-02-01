19ecdab2db1aa2fc82b8cb455884774b7054fd87
Oumarou Maiga <oumar@mac.home>
2025-02-01 16:27:13 +0100

    Complete Pong game

--- OOP-Python/20 & 21 - Snake game/food.py
+++ OOP-Python/20 & 21 - Snake game/food.py
@@ -6,7 +6,7 @@ class Food(Turtle):
         super().__init__()
         self.penup()
         self.shape('circle')
-        self.shapesize(stretch_len=0.25, stretch_wid=0.25)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color('blue')
         self.speed('fastest')
         self.refresh()
