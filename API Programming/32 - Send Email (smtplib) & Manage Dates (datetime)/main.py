3b67b59e62a2bca39a1f1905e5c242438f196d55
Oumarou Maiga <oumar@MacBook-Air-de-Oumarou.local>
2025-02-04 20:58:21 +0100

    Complete day 33

--- API Programming/32 - Send Email (smtplib) & Manage Dates (datetime)/main.py
+++ API Programming/32 - Send Email (smtplib) & Manage Dates (datetime)/main.py
@@ -15,13 +15,11 @@ current_date = dt.datetime.now()
 # 2. Check if today matches a birthday in the birthdays.csv
 for row in data.itertuples():
     if (int(row.month), int(row.day)) == (current_date.month, current_date.day):
-        print('Anniv!')
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
         random_letter_num = random.randint(1, 3)
         with open(f'letter_templates/letter_{random_letter_num}.txt') as file:
             letter = file.read()
             letter = letter.replace('[NAME]', row.name)
-            print(letter)
 # 4. Send the letter generated in step 3 to that person's email address.
         with smtplib.SMTP("smtp.gmail.com", 587) as connection:
             connection.starttls()
